1b2d52d0df81c181daf39a7c3050cdf30c9e6033
masx200 <34191203+masx200@users.noreply.github.com>
2020-05-27 23:23:48 +0800

    添加了命令行帮助

--- dist/cli.js
+++ dist/cli.js
@@ -1,9 +1,8 @@
 #!/usr/bin/env node
+import argsobj from "./parsed-cli-options.js";
 import process from "process";
 import loadjson from "./加载JSON.js";
 import { start } from "./index.js";
-import { parseargs } from "./parse-args.js";
-const argsobj = parseargs(process.argv.slice(2));
 const { input, output } = argsobj;
 const filenameurl = import.meta.url;
 const jsonurl = new URL("../image-config.json", filenameurl);
@@ -25,10 +24,10 @@ else {
 必选参数 \`output\`:类型\`string\`,输出图片目录 
     
 可选参数 \`maxpixels\`:类型\`number\`,输出图片最大像素数 
-    `);
+可选参数 concurrent:类型:number,同时运行的图片处理进程最大值`);
     console.error("示例:");
     console.error(`node ./dist/cli.js --input=D:/baidupandownload/图片输入/  --output=D:/baidupandownload/暴力切割图片/`);
-    console.error(`node ./dist/cli.js --input=D:/baidupandownload/图片输入  --output=D:/baidupandownload/输出切割图片/ --maxpixels=4000000`);
+    console.error(`node ./dist/cli.js --input=D:/baidupandownload/图片输入  --output=D:/baidupandownload/输出切割图片/ --maxpixels=4000000 --concurrent=4`);
     console.error("输入的参数有误!");
     process.exit(1);
 }
