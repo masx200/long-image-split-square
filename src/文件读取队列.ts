b18724a504189be0492ccb1be29d9dfa3cc7b6ff
masx200 <34191203+masx200@users.noreply.github.com>
2020-05-31 23:58:22 +0800

    Update 文件读取队列.ts

--- src/文件读取队列.ts
+++ src/文件读取队列.ts
@@ -3,10 +3,11 @@ const 同时读取的最大文件数 = 20;
 const 文件读取队列 = 创建异步限流队列(同时读取的最大文件数);
 export default 文件读取队列;
 import { statusdata } from "@masx200/async-task-current-limiter";
-const asynclimiter = 文件读取队列;
+/*const asynclimiter = 文件读取队列;
 const listener = (data: statusdata) =>
     console.log("文件读取限流" + JSON.stringify(data));
 
 asynclimiter.target.on("free", listener);
 
 asynclimiter.target.on("full", listener);
+*/
