6c8e2b84ab0efd0e0709316d2a0cc0a3f45da1af
masx200 <masx200@qq.com>
2024-12-20 09:04:58 +0800

    Update 异步限流-img2webp.ts

--- src/异步限流-img2webp.ts
+++ src/异步限流-img2webp.ts
@@ -18,7 +18,7 @@ export default asyncwrap(img2webp);
 async function img2webp(input: string, output: string) {
     let execout = await execpromise(
         String(argsobj["cwebp-path"] ?? getBin("cwebp")),
-        ["-o", output, "-v", "--", input],
+        ["-o", output, "-v", "--", input]
     );
     return execout;
 }
